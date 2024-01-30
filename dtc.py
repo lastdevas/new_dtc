805af0b80f160e0dede30fdecff8604d58efc26b
Farisan Haq <152673014+lastdevas@users.noreply.github.com>
2024-01-30 14:59:09 +0700

    Update dtc.py

--- dtc.py
+++ dtc.py
@@ -13,8 +13,25 @@ def detect_objects(image_tensor):
     detector_output = detector(image_tensor)
     return detector_output
 
+def preprocess_image(uploaded_file):
+    # Convert the uploaded image to a format that the model expects
+    image = Image.open(uploaded_file).convert("RGB")
+
+    # Resize the image to the expected size
+    resized_image = image.resize((224, 224))
+    
+    # Convert the resized image to a numpy array
+    image_array = np.array(resized_image)
+    
+    # Normalize the pixel values to be between 0 and 1
+    image_array = image_array / 255.0
+
+    # Add an extra dimension to the array to represent the batch size
+    image_tensor = tf.convert_to_tensor([image_array], dtype=tf.float32)
+    return image_tensor
+
 def main():
-    st.title("Object Detection with TensorFlow Hub")
+    st.title("Engine Image Detection")
 
     # File uploader
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -23,20 +40,8 @@ def main():
         # Display the uploaded image
         st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
-        # Convert the uploaded image to a format that the model expects
-        image = Image.open(uploaded_file).convert("RGB")
-
-        # Resize the image to the expected size
-        resized_image = image.resize((224, 224))
-        
-        # Convert the resized image to a numpy array
-        image_array = np.array(resized_image)
-        
-        # Normalize the pixel values to be between 0 and 1
-        image_array = image_array / 255.0
-
-        # Add an extra dimension to the array to represent the batch size
-        image_tensor = tf.convert_to_tensor([image_array])
+        # Convert and preprocess the uploaded image
+        image_tensor = preprocess_image(uploaded_file)
 
         # Detect objects in the image
         detection_result = detect_objects(image_tensor)
@@ -46,7 +51,6 @@ def main():
         probabilities = detection_result['probabilities']
         for name, prob in zip(class_names, probabilities):
             st.write(f"{name}: {prob:.2%}")
-
 
 if __name__ == "__main__":
     main()
